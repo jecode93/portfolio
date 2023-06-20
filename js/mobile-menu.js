7f6388e08714d0d5cfae1383287383ff8bb526bc
jecode93 <jeanemmanuelcadet@gmail.com>
2023-06-19 20:17:56 -0400

    Created mobile menu with effect to show or not using javaScript

---
+++ js/mobile-menu.js
@@ -0,0 +1,16 @@
+const humburger = document.querySelector('.hamburger-icon');
+const mobile = document.querySelector('.mobile-menu');
+const close = document.querySelector('.close');
+const link = document.querySelector('.mobile-link');
+
+humburger.addEventListener("click", () => {
+	mobile.classList.toggle("active")
+})
+
+close.addEventListener("click", () => {
+	mobile.classList.toggle("active")
+})
+
+link.addEventListener("click", () => {
+	mobile.classList.toggle("active")
+})
