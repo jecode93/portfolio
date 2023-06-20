4157ac3aed27ca46689f10bad87778b704baab17
jecode93 <jeanemmanuelcadet@gmail.com>
2023-06-20 09:41:32 -0400

    Make changes as request by linters

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -3,14 +3,14 @@ const mobile = document.querySelector('.mobile-menu');
 const close = document.querySelector('.close');
 const link = document.querySelector('.mobile-link');
 
-humburger.addEventListener('click', ()=>{
-mobile.classList.toggle('active');
+humburger.addEventListener('click', () => {
+  mobile.classList.toggle('active');
 });
 
-close.addEventListener('click', ()=>{
-mobile.classList.toggle('active');
+close.addEventListener('click', () => {
+  mobile.classList.toggle('active');
 });
 
-link.addEventListener('click', ()=>{
-mobile.classList.toggle('active');
+link.addEventListener('click', () => {
+  mobile.classList.toggle('active');
 });
