1b085ca46c21feb0de61e9aa8c1581047355534b
jecode93 <jeanemmanuelcadet@gmail.com>
2023-06-22 10:09:19 -0400

    Make changes as requested by Linters

--- js/validate-form.js
+++ js/validate-form.js
@@ -6,63 +6,62 @@ const renderElement = document.querySelector('.render');
 
 // Check if the email is lowercase
 function checkEmail() {
-	const email = document.getElementById('email').value;
-	if (email.toString().toLowerCase() !== email) {
-		return false;
-	} else {
-		return true;
-	};
+  const email = document.getElementById('email').value;
+  if (email.toString().toLowerCase() !== email) {
+    return false;
+  }
+    return true;
 }
 
 // Remove error after 5 seconds
 function removeError() {
-	setTimeout(() => {
-		renderElement.classList.remove('error');
-		renderElement.classList.remove('success');
-		renderElement.innerHTML = '';
-	}, 5000);
+  setTimeout(() => {
+    renderElement.classList.remove('error');
+    renderElement.classList.remove('success');
+    renderElement.innerHTML = '';
+  }, 5000);
 }
 
 // Function to validate the form
 function validateForm() {
-	const name = document.getElementById('fullname');
-	const email = document.getElementById('email')
-	const message = document.getElementById('message');
+  const name = document.getElementById('fullname');
+  const email = document.getElementById('email')
+  const message = document.getElementById('message');
 
-	if (name.value === '' || email.value === '' || message.value === '') {
-		renderElement.classList.add('error');
-		renderElement.innerHTML = 'All fields are required';
-		removeError();
-		return false;
-	}
+  if (name.value === '' || email.value === '' || message.value === '') {
+    renderElement.classList.add('error');
+    renderElement.innerHTML = 'All fields are required';
+    removeError();
+    return false;
+ }
 
 	if (!checkEmail()) {
-		renderElement.classList.add('error');
-		renderElement.innerHTML = 'Email must be lowercase';
-		removeError();
-		return false;
-	}
+    renderElement.classList.add('error');
+    renderElement.innerHTML = 'Email must be lowercase';
+    removeError();
+    return false;
+ }
 
-	return true;
+  return true;
 }
 
 // Function to submit the form
 function submitForm() {
-	formElement.addEventListener('submit', (e) => {
-		e.preventDefault();
-		if (validateForm()) {
-			// submit the form
-			formElement.submit();
-			document.getElementById('fullname').value = '';
-			document.getElementById('email').value = '';
-			document.getElementById('message').value = '';
-			renderElement.classList.remove('error');
-			renderElement.classList.remove('isHidden');
-			renderElement.classList.add('success');
-			renderElement.innerHTML = 'Your message has been sent';
-			removeError();
-		}
-	});
+  formElement.addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      // submit the form
+      formElement.submit();
+      document.getElementById('fullname').value = '';
+      document.getElementById('email').value = '';
+      document.getElementById('message').value = '';
+      renderElement.classList.remove('error');
+      renderElement.classList.remove('isHidden');
+      renderElement.classList.add('success');
+      renderElement.innerHTML = 'Your message has been sent';
+      removeError();
+    }
+  });
 }
 
 submitForm();
